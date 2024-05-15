35357b9e8c5d8550ec3ab09d2c3e1ed207dbe8d7
Eweb a1professionals <a1pro>
2024-05-15 11:00:16 +0530

    booking form get api done

--- src/component/BookingForm.js
+++ src/component/BookingForm.js
@@ -16,7 +16,10 @@ import styles from '../styles/Styles';
 import {Formik} from 'formik';
 import * as yup from 'yup';
 import {useNavigation} from '@react-navigation/native';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+import {Base_url} from '../Apiurl';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const validationSchema = yup.object().shape({
   required_cleaners: yup.string().required('Cleaners are required'),
@@ -29,6 +32,10 @@ const BookingForm = () => {
   const navigation = useNavigation();
   const [date, setDate] = useState(new Date());
   const [show, setShow] = useState(false);
+  const [cleanersData, setCleanersData] = useState([]);
+  const [duration, setDuration] = useState([]);
+  const [bookingTypes, setBookingTypes] = useState([]);
+  const [timeSlot, setTimeSlot] = useState([]);
 
   const onChange = (event, selectedDate) => {
     const currentDate = selectedDate || date;
@@ -50,6 +57,45 @@ const BookingForm = () => {
     let year = date.getFullYear();
     return `${day}/${month}/${year}`;
   };
+
+  // getbooking form value
+  const getbookingvalue = async () => {
+    const token = await AsyncStorage.getItem('token');
+    if (!token) {
+      console.error('No token found');
+      return;
+    }
+    try {
+      const res = await axios({
+        method: 'GET',
+        url: Base_url.getformvalues,
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + token,
+        },
+      });
+      if (res.data.success === true) {
+        console.log(res.data.message);
+        setCleanersData(res.data.data.cleaners);
+        setDuration(res.data.data.duration);
+        setBookingTypes(res.data.data.booking_types);
+        setTimeSlot(res.data.data.time_slots);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getbookingvalue();
+  }, []);
+
+  useEffect(() => {
+    console.log('cleaners', cleanersData);
+    console.log('duration', duration);
+    console.log('booking types', bookingTypes);
+    console.log('time slots', timeSlot);
+  }, [cleanersData, duration, bookingTypes, timeSlot]);
   return (
     <ScrollView contentContainerStyle={styles.scrollContainer}>
       <ImageBackground
@@ -85,7 +131,6 @@ const BookingForm = () => {
           validationSchema={validationSchema}
           onSubmit={values => handleSubmit(values)}>
           {({
-            selectedValue,
             handleChange,
             handleBlur,
             handleSubmit,
@@ -102,9 +147,14 @@ const BookingForm = () => {
                   onValueChange={handleChange('required_cleaners')}
                   onBlur={handleBlur('required_cleaners')}>
                   <Picker.Item label="Select One" value="" />
-                  <Picker.Item label="Option 1" value="option1" />
-                  <Picker.Item label="Option 2" value="option2" />
-                  <Picker.Item label="Option 3" value="option3" />
+                  {cleanersData &&
+                    cleanersData.map((data, index) => (
+                      <Picker.Item
+                        key={index}
+                        label={data.fname}
+                        value={data.fname}
+                      />
+                    ))}
                 </Picker>
                 {touched.required_cleaners && errors.required_cleaners && (
                   <Text style={styles.errortext}>
@@ -120,9 +170,14 @@ const BookingForm = () => {
                   onValueChange={handleChange('booking_type')}
                   onBlur={handleBlur('booking_type')}>
                   <Picker.Item label="Booking Type" value="" />
-                  <Picker.Item label="Option 1" value="option1" />
-                  <Picker.Item label="Option 2" value="option2" />
-                  <Picker.Item label="Option 3" value="option3" />
+                  {bookingTypes &&
+                    bookingTypes.map((data, index) => (
+                      <Picker.Item
+                        key={index}
+                        label={data.name_type}
+                        value={data.name_type}
+                      />
+                    ))}
                 </Picker>
                 {touched.booking_type && errors.booking_type && (
                   <Text style={styles.errortext}>{errors.booking_type}</Text>
@@ -169,7 +224,14 @@ const BookingForm = () => {
                   onValueChange={handleChange('duration')}
                   onBlur={handleBlur('duration')}>
                   <Picker.Item label="Select Duration" value="" />
-                  <Picker.Item label="Option 1" value="option1" />
+                  {duration &&
+                    duration.map((data, index) => (
+                      <Picker.Item
+                        key={index}
+                        label={data.hours}
+                        value={data.hours}
+                      />
+                    ))}
                   <Picker.Item label="Option 2" value="option2" />
                   <Picker.Item label="Option 3" value="option3" />
                 </Picker>
@@ -185,9 +247,14 @@ const BookingForm = () => {
                   onValueChange={handleChange('start_time')}
                   onBlur={handleBlur('start_time')}>
                   <Picker.Item label="Select Start Time" value="" />
-                  <Picker.Item label="Option 1" value="option1" />
-                  <Picker.Item label="Option 2" value="option2" />
-                  <Picker.Item label="Option 3" value="option3" />
+                  {timeSlot &&
+                    timeSlot.map((data, index) => (
+                      <Picker.Item
+                        key={index}
+                        label={data.time_bw}
+                        value={data.time_bw}
+                      />
+                    ))}
                 </Picker>
                 {touched.start_time && errors.start_time && (
                   <Text style={styles.errortext}>{errors.start_time}</Text>
