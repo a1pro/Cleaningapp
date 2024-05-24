87d02f2e9be0da1891704cc134f5c32a7dde6e47
Eweb a1professionals <a1pro>
2024-05-24 13:34:38 +0530

    fix

--- src/component/MyOrders.js
+++ src/component/MyOrders.js
@@ -1,9 +1,10 @@
-import {Text, TouchableOpacity, View} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
 import styles from '../styles/Styles';
-import {useNavigation} from '@react-navigation/native';
-import {useDispatch, useSelector} from 'react-redux';
-import {useEffect, useState} from 'react';
-import {getcleanerorder} from '../redux/CleanerOrderSlice';
+import { useNavigation } from '@react-navigation/native';
+import { useDispatch, useSelector } from 'react-redux';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { getcleanerorder } from '../redux/CleanerOrderSlice';
 import { getUserBooking } from '../redux/MybookingSlice';
 
 const MyOrders = () => {
@@ -14,33 +15,29 @@ const MyOrders = () => {
   // Cleaner Orders Data
   const ongoing_data = useSelector(state => state.cleanerorder.cleanerOrderdata?.ongoing_data);
   const completed = useSelector(state => state.cleanerorder.cleanerOrderdata?.completed_data);
-  // Cleaner Orders end
 
   // User Orders Data
-  const userOngoing_data = useSelector(state=>state.userbookingdata.userbooking?.ongoing_count);
-  const userCompleted_data = useSelector(state=>state.userbookingdata.userbooking?.completed_count);
+  const userOngoing_data = useSelector(state => state.userbookingdata.userbooking?.ongoing_count);
+  const userCompleted_data = useSelector(state => state.userbookingdata.userbooking?.completed_count);
 
-
-
-  //get cleaner order details
+  // Get cleaner order details
   useEffect(() => {
     dispatch(getcleanerorder());
   }, [dispatch]);
 
-  //get User Order Details
-  useEffect(()=>{
-    dispatch(getUserBooking())
-  },[dispatch]);
+  // Get user order details
+  useEffect(() => {
+    dispatch(getUserBooking());
+  }, [dispatch]);
 
-  
-  //Cehck Role and show screen accroding role
+  // Check Role and show screen according to role
   useEffect(() => {
     const checkRoles = async () => {
       try {
         const storedRoles = await AsyncStorage.getItem('role');
         if (storedRoles !== null) {
           setRoles(storedRoles);
-          console.log('roles124', roles);
+          console.log('roles124', storedRoles);
         } else {
           console.log('No roles found');
         }
@@ -51,44 +48,43 @@ const MyOrders = () => {
     checkRoles();
   }, []);
 
+  if (roles === null) {
+    return <Text>Loading...</Text>;
+  }
+
   return (
-  <>
-    {roles === "1" ?<>
-    {/* For Cleaners */}
-    <View style={{marginTop: 20}}>
-      {/* <TouchableOpacity onPress={()=>navigation.navigate('MyOrderPage')}> */}
-      <Text style={styles.h6}>My Orders</Text>
-      {/* </TouchableOpacity> */}
-      <View style={styles.orders_wrapper}>
-        <View style={styles.ordersbox}>
-          <Text style={styles.h3}>{ongoing_data?.length}</Text>
-          <Text style={styles.h6}>Ongoing</Text>
+    <>
+      {roles === "1" ? (
+        <View style={{ marginTop: 20 }}>
+          <Text style={styles.h6}>My Orders</Text>
+          <View style={styles.orders_wrapper}>
+            <View style={styles.ordersbox}>
+              <Text style={styles.h3}>{ongoing_data?.length}</Text>
+              <Text style={styles.h6}>Ongoing</Text>
+            </View>
+            <View style={styles.ordersbox}>
+              <Text style={styles.h3}>{completed?.length}</Text>
+              <Text style={styles.h6}>Completed</Text>
+            </View>
+          </View>
         </View>
-        <View style={styles.ordersbox}>
-          <Text style={styles.h3}>{completed?.length}</Text>
-          <Text style={styles.h6}>Complated</Text>
+      ) : (
+        <View style={{ marginTop: 20 }}>
+          <Text style={styles.h6}>My Orders</Text>
+          <View style={styles.orders_wrapper}>
+            <View style={styles.ordersbox}>
+              <Text style={styles.h3}>{userOngoing_data}</Text>
+              <Text style={styles.h6}>Ongoing</Text>
+            </View>
+            <View style={styles.ordersbox}>
+              <Text style={styles.h3}>{userCompleted_data}</Text>
+              <Text style={styles.h6}>Completed</Text>
+            </View>
+          </View>
         </View>
-      </View>
-    </View>
-    </>:<>
-    {/* For Users */}
-    <View style={{marginTop: 20}}>
-      {/* <TouchableOpacity onPress={()=>navigation.navigate('MyOrderPage')}> */}
-      <Text style={styles.h6}>My Orders</Text>
-      {/* </TouchableOpacity> */}
-      <View style={styles.orders_wrapper}>
-        <View style={styles.ordersbox}>
-          <Text style={styles.h3}>{userOngoing_data}</Text>
-          <Text style={styles.h6}>Ongoing</Text>
-        </View>
-        <View style={styles.ordersbox}>
-          <Text style={styles.h3}>{userCompleted_data}</Text>
-          <Text style={styles.h6}>Complated</Text>
-        </View>
-      </View>
-    </View>
-    </>}
-  </>
+      )}
+    </>
   );
 };
+
 export default MyOrders;
